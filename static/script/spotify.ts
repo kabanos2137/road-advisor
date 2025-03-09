4b299e507729ae998e17d9fd1a629d023668c5e0
kabanos2137 <gaweljar0@outlook.com>
2025-03-09 17:47:15 +0100

    Create home page

--- static/script/spotify.ts
+++ static/script/spotify.ts
@@ -4,6 +4,13 @@ let main = document.querySelector("main") as HTMLElement;
 let nav = document.querySelector("nav") as HTMLElement;
 let footer = document.querySelector("footer") as HTMLElement;
 let currentlySelected: HTMLElement | null = null;
+let clicked = false;
+
+function moveLastToFirst(arr: any[]): any[] {
+    const lastElement = arr.splice(arr.length - 1, 1)[0];
+    arr.unshift(lastElement);
+    return arr;
+}
 
 const switchToSpotify = () => {
     main.classList.add("mid-stage-2");
@@ -63,6 +70,7 @@ const displaySpotify = (data: {token: string}) => {
         </section>
     `;
     footer.innerHTML = ``;
+    openPageHome();
 
     currentlySelected = document.querySelector(".spotify-selected");
 
@@ -73,6 +81,12 @@ const displaySpotify = (data: {token: string}) => {
                 currentlySelected.classList.remove("spotify-selected");
             }
             element.classList.add("spotify-selected");
+
+            switch(element.id){
+                case "spotify-sidebar-button-home":
+                    openPageHome();
+            }
+
             currentlySelected = element;
         });
     });
@@ -80,4 +94,255 @@ const displaySpotify = (data: {token: string}) => {
     setTimeout(() => {
         main.classList.remove("mid-stage-1");
     }, 300);
+}
+
+const openPageHome = () => {
+    let spotifyCenter = document.querySelector("#spotify-center") as HTMLElement;
+
+    fetch("https://api.spotify.com/v1/me", {
+        headers: {
+            Authorization: `Bearer ${authTokenSpotify}`
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+            spotifyCenter.innerHTML = `
+                <div class="spotify-recommendation" id="spotify-recommendation-tracks">
+                    <h1>Hello, ${data.display_name}!</h1>
+                    <h5>Top Tracks</h5>
+                    <div class="spotify-recommendation-list">
+                        <span class="material-symbols-outlined">chevron_left</span>
+                        <div class="spotify-recommendation-item"></div>
+                        <div class="spotify-recommendation-item"></div>
+                        <div class="spotify-recommendation-item"></div>
+                        <span class="material-symbols-outlined">chevron_right</span>
+                    </div>
+                </div>
+                <div class="spotify-recommendation" id="spotify-recommendation-artists" style="margin-bottom: 30px;">
+                    <h5>Top Artists</h5>
+                    <div class="spotify-recommendation-list">
+                        <span class="material-symbols-outlined">chevron_left</span>
+                        <div class="spotify-recommendation-item"></div>
+                        <div class="spotify-recommendation-item"></div>
+                        <div class="spotify-recommendation-item"></div>
+                        <span class="material-symbols-outlined">chevron_right</span>
+                    </div>
+                </div>
+            `;
+            spotifyRecommendationPopulate();
+        });
+}
+
+const spotifyRecommendationPopulate = () => {
+    fetch("https://api.spotify.com/v1/me/top/tracks", {
+        headers: {
+            Authorization: `Bearer ${authTokenSpotify}`
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+            let topTracks: HTMLElement[] = [];
+
+            data.items.forEach((item: any) => {
+                let div = document.createElement("div");
+                div.classList.add("spotify-recommendation-item");
+
+                div.style.position = "relative";
+                div.style.overflow = "hidden";
+
+                let backgroundDiv = document.createElement("div");
+
+                backgroundDiv.style.backgroundImage = `url(${item.album.images[0].url})`;
+                backgroundDiv.style.backgroundSize = "cover";
+                backgroundDiv.style.backgroundPosition = "center";
+                backgroundDiv.style.filter = "blur(8px)";
+
+                backgroundDiv.style.position = "absolute";
+                backgroundDiv.style.top = "-10px";
+                backgroundDiv.style.left = "-10px";
+                backgroundDiv.style.right = "-10px";
+                backgroundDiv.style.bottom = "-10px";
+                backgroundDiv.style.zIndex = "0";
+
+                backgroundDiv.style.opacity = "0.7";
+
+                div.appendChild(backgroundDiv);
+
+                let contentContainer = document.createElement("div");
+                contentContainer.classList.add("spotify-content-container");
+                contentContainer.style.position = "relative";
+                contentContainer.style.zIndex = "1";
+                contentContainer.style.padding = "10px";
+                contentContainer.style.display = "flex";
+                contentContainer.style.flexDirection = "column";
+                contentContainer.style.alignItems = "center";
+                contentContainer.style.justifyContent = "center";
+
+
+                contentContainer.innerHTML = `
+                    <h1></h1>
+                    <h2>${item.name}</h2>
+                    <h3>${item.artists[0].name}</h3>    
+                `
+
+                div.appendChild(contentContainer);
+
+                topTracks.push(div);
+            });
+
+            topTracks = moveLastToFirst(topTracks);
+
+            let list = document.querySelector("#spotify-recommendation-tracks > .spotify-recommendation-list") as HTMLElement;
+
+            list.innerHTML = "<span id='spotify-recommendation-tracks-left' class=\"material-symbols-outlined\">chevron_left</span>";
+
+            topTracks.forEach((track: HTMLElement, index: number) => {
+                if(index === 0){
+                    track.children[1].children[0].innerHTML = `${topTracks.length}.`
+                    track.classList.add("spotify-recommendation-item-hidden", `spotify-recommendation-item-${index}`);
+                }else if(index < 4){
+                    track.children[1].children[0].innerHTML = `${index}.`
+                    track.classList.add(`spotify-recommendation-item-${index}`);
+                }else{
+                    track.children[1].children[0].innerHTML = `${index}.`;
+                    track.classList.add("spotify-recommendation-item-hidden", `spotify-recommendation-item-${index}`);
+                }
+                list.appendChild(track);
+            });
+
+            list.innerHTML += "<span id='spotify-recommendation-tracks-right' class=\"material-symbols-outlined\">chevron_right</span>";
+
+            let left = document.querySelector("#spotify-recommendation-tracks-left") as HTMLElement;
+            let right = document.querySelector("#spotify-recommendation-tracks-right") as HTMLElement;
+
+            right.addEventListener("click", () => {
+                if(!clicked) {
+                    clicked = true;
+                    list.children[2].classList.add("spotify-recommendation-item-hidden");
+                    list.children[5].classList.remove("spotify-recommendation-item-hidden");
+                    list.insertBefore(list.children[1], list.children[list.children.length - 1]);
+                    setTimeout(() => {
+                        clicked = false;
+                    }, 300)
+                }
+            });
+
+            left.addEventListener("click", () => {
+                if(!clicked){
+                    clicked = true;
+                    list.children[5].classList.add("spotify-recommendation-item-hidden");
+                    list.children[2].classList.remove("spotify-recommendation-item-hidden");
+                    list.insertBefore(list.children[list.children.length - 2], list.children[2]);
+                    setTimeout(() => {
+                        clicked = false;
+                    }, 300)
+                }
+            });
+        });
+    fetch("https://api.spotify.com/v1/me/top/artists", {
+        headers: {
+            Authorization: `Bearer ${authTokenSpotify}`
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+            let topArtists: HTMLElement[] = [];
+            data.items.forEach((item: any) => {
+                let div = document.createElement("div");
+                div.classList.add("spotify-recommendation-item");
+
+                div.style.position = "relative";
+                div.style.overflow = "hidden";
+
+                let backgroundDiv = document.createElement("div");
+
+                backgroundDiv.style.backgroundImage = `url(${item.images[0].url})`;
+                backgroundDiv.style.backgroundSize = "cover";
+                backgroundDiv.style.backgroundPosition = "center";
+                backgroundDiv.style.filter = "blur(8px)";
+
+                backgroundDiv.style.position = "absolute";
+                backgroundDiv.style.top = "-10px";
+                backgroundDiv.style.left = "-10px";
+                backgroundDiv.style.right = "-10px";
+                backgroundDiv.style.bottom = "-10px";
+                backgroundDiv.style.zIndex = "0";
+
+                backgroundDiv.style.opacity = "0.7";
+
+                div.appendChild(backgroundDiv);
+
+                let contentContainer = document.createElement("div");
+                contentContainer.classList.add("spotify-content-container");
+                contentContainer.style.position = "relative";
+                contentContainer.style.zIndex = "1";
+                contentContainer.style.padding = "10px";
+                contentContainer.style.display = "flex";
+                contentContainer.style.flexDirection = "column";
+                contentContainer.style.alignItems = "center";
+                contentContainer.style.justifyContent = "center";
+
+
+                contentContainer.innerHTML = `
+                    <h1></h1>
+                    <h2>${item.name}</h2> 
+                `
+
+                div.appendChild(contentContainer);
+
+                topArtists.push(div);
+            });
+
+            console.log(topArtists);
+
+            topArtists = moveLastToFirst(topArtists);
+
+            let list = document.querySelector("#spotify-recommendation-artists > .spotify-recommendation-list") as HTMLElement;
+
+            list.innerHTML = "<span id='spotify-recommendation-artists-left' class=\"material-symbols-outlined\">chevron_left</span>";
+
+            topArtists.forEach((track: HTMLElement, index: number) => {
+                if(index === 0){
+                    track.children[1].children[0].innerHTML = `${topArtists.length}.`
+                    track.classList.add("spotify-recommendation-item-hidden", `spotify-recommendation-item-${index}`);
+                }else if(index < 4){
+                    track.children[1].children[0].innerHTML = `${index}.`
+                    track.classList.add(`spotify-recommendation-item-${index}`);
+                }else{
+                    track.children[1].children[0].innerHTML = `${index}.`;
+                    track.classList.add("spotify-recommendation-item-hidden", `spotify-recommendation-item-${index}`);
+                }
+                console.log(track.children[1].children[0].innerHTML)
+                list.appendChild(track);
+            });
+
+            list.innerHTML += "<span id='spotify-recommendation-artists-right' class=\"material-symbols-outlined\">chevron_right</span>";
+
+            let left = document.querySelector("#spotify-recommendation-artists-left") as HTMLElement;
+            let right = document.querySelector("#spotify-recommendation-artists-right") as HTMLElement;
+
+            right.addEventListener("click", () => {
+                if(!clicked) {
+                    clicked = true;
+                    list.children[2].classList.add("spotify-recommendation-item-hidden");
+                    list.children[5].classList.remove("spotify-recommendation-item-hidden");
+                    list.insertBefore(list.children[1], list.children[list.children.length - 1]);
+                    setTimeout(() => {
+                        clicked = false;
+                    }, 300)
+                }
+            });
+
+            left.addEventListener("click", () => {
+                if(!clicked){
+                    clicked = true;
+                    list.children[5].classList.add("spotify-recommendation-item-hidden");
+                    list.children[2].classList.remove("spotify-recommendation-item-hidden");
+                    list.insertBefore(list.children[list.children.length - 2], list.children[2]);
+                    setTimeout(() => {
+                        clicked = false;
+                    }, 300)
+                }
+            });
+        })
 }
